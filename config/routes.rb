2344188973aec663f84dd26dfab2a236e743b038
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-03 00:38:42 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :admins, :controllers => {
-    :sessions => 'admins/sessions'
-  }
 
-  devise_for :cusomers, :controllers => {
-    :sessions => 'customers/sessions',
-    :registrations => "cusomers/registrations"
-  }
-  root to: 'public/products#top'
-  get 'about' => 'public/products#about'
 end
