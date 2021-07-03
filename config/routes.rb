617c431e0f5b1864fa6534741eb7a2dbae613015
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-03 13:56:15 +0000

    [Add routing users/admin]

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,28 @@ devise_for :users,controllers: {
   sessions: 'users/sessions'
 }
 
+scope module: :public do
+  root to: 'products#top'
+  get 'about' => 'products#about'
+  resources 'products', only: [:index, :show]
+  resources 'users', only: [:index, :edit]
+  get 'users/out' => 'users#out'
+  get 'users/withdraw' => 'users#withdraw'
+  delete 'carts_items/destroy_all' => 'cart_items#destroy_all'
+  resources 'carts_items', only: [:index, :create, :update, :destroy]
+  post 'orders/check' => 'orders#check'
+  get 'orders/complete' => 'orders#complete'
+  resources :orders, only: [:index, :show, :new, :create]
+ end
+
+ namespace :admin do
+  get 'orders' => 'orders#top', as: :root
+  resources :orders, only: [:show, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :ordered_products, only: [:update]
+  resources :products, except: [:destroy]
+  resources :genres, except: [:destroy]
+  get 'searches' => "searches"
+ end
+
 end
