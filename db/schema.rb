09c1959ca7a6680e0777feb94a352842eeff721f
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-09 06:37:04 +0000

    [Add admin_customer]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_041537) do
+ActiveRecord::Schema.define(version: 2021_07_09_062116) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -109,6 +109,11 @@ ActiveRecord::Schema.define(version: 2021_07_09_041537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "kana_first_name"
+    t.string "kana_last_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
