b08afbd89ac5b508c7816166accb68eb395cd761
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-09 03:00:23 +0000

    [make products index/new]

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,16 +1,51 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_admin!
+
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: '登録を成功しました'
+    else
+       @product = Product.new
+       @genres  = Genre.all
+       flash.now[:danger] = '空欄があります。'
+       render 'new'
+    end
   end
 
   def new
+      @product = Product.new
+      @genres = Genre.all
   end
 
   def index
+      @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    #@product_intax = ((@product.price * 1.1).round(2)).ceil
   end
 
   def edit
+    @product = Product.find(params[:id])
+    @genres  = Genre.all
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: 'You have updated product successfully.'
+    else
+      @product = Product.find(params[:id])
+      @genres = Genre.all
+      flash.now[:danger] = ' 空欄があります。'
+      render 'edit'
+    end
   end
+
+  private
+   def product_params
+     params.require(:product).permit(:name, :image_id, :description, :price, :genre_id, :status, :genre_id)
+   end
 end
