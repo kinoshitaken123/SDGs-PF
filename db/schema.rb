bafd02a09434d9eaa9b4613e2e0f111e125ebd24
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-12 01:58:40 +0000

    [fix product いいね・コメント表示]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_135503) do
+ActiveRecord::Schema.define(version: 2021_07_11_124324) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 2021_07_10_135503) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
