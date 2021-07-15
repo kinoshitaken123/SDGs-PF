b97c4ab4e548fe0c93972f161293aa843bad5ffe
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-15 11:09:58 +0000

    [fix]

--- db/migrate/20210714133358_add_rate_to_products_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddRateToProductsComments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products_comments, :rate, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_133358) do
+ActiveRecord::Schema.define(version: 2021_07_14_113107) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 2021_07_14_133358) do
     t.text "comment"
     t.integer "product_id"
     t.integer "user_id"
-    t.float "rate"
     t.integer "book_id"
   end
 
