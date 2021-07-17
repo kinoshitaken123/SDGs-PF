9332c7eb04374e45ea74c802599e1bcb88568bf1
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-17 13:35:08 +0000

    [make chart room]

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,7 +1,31 @@
 class Admin::RoomsController < ApplicationController
+
+  def create
+      chat = Chat.new(chats_params)
+    if chat.save
+      redirect_to admin_rooms_path,success: 'メッセージを送信しました'
+    else
+      redirect_to admin_rooms_path,alert: 'メッセージを送信できませんでした'
+    end
+  end
+
   def index
   end
 
   def show
+    @room = Room.find(params[:id])
+    if UserRoom.where(:user_id => current_user.id, :room_id => @room.id).present?
+      @chats = @room.chats
+      @chat =Chat.new
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
+  
+  private
+  
+  def chat_params
+    params.require(:chat).permit(:message, :room_id).merge(admin_id: current_admin.id)
+  end
+
 end
