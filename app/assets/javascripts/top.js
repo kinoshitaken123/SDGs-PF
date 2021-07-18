9bcbac00a7499929f805cbaaf1f404affdb2d408
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-18 03:12:09 +0000

    [top view もっとみる追加]

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -13,3 +13,10 @@ $(document).ready(function(){
   });
 });
 });
+
+$(function(){
+  $(".more").on("click", function() {
+    $(this).toggleClass("on-click");
+    $(".txt-hide").slideToggle(1000);
+  });
+});
