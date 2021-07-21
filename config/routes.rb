c280c0f6a32b89ce7e35e95b5285e1c51b396012
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-21 06:34:15 +0000

    [ユーザーゲスト　ログイン]

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
    registrations: 'users/registrations'
  }
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
  devise_for :admins, controllers: {
    sessions: 'admins/sessions',
    passwords:     'admins/passwords',
