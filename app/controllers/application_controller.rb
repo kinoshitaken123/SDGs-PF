1fd2b45c4def39a262666c6ccbaf955a1244a433
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-24 03:45:06 +0000

    [toplayout修正]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   add_flash_types :success, :info, :warning, :danger
 
