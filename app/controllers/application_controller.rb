7f7757cf9357c067749d5a933ff8ccc34298ceb8
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-26 07:07:41 +0000

    [Add image]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  
+  
 
  private
 
