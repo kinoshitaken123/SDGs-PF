5aa042995536eb7445a462685784fa8282e529a6
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-26 10:48:14 +0000

    [chat エラー解消]

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -8,11 +8,12 @@ class Public::RoomsController < ApplicationController
 
   def show
       @room = Room.find_or_create_by(id: params[:id])
+      @product_id = params[:product_id]
       @chat =Chat.new
     if !@room.nil? && UserRoom.where(:user_id => current_user.id, :room_id => params[:id]).present? #roomがnilじゃなかったらなおかつuser_room処理を実行
       @chats = @room.chats
-        
-      logger.info('&&&&&' + @chats.inspect)
+
+      #logger.info('&&&&&' + @chats.inspect)
     else
       @user_room = UserRoom.new({user_id: current_user.id, room_id: @room.id})
       @user_room.save
