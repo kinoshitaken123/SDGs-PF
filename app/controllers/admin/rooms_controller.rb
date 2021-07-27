5b5763be6d3874f683a708dfe0a72a9f61a9458c
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-27 07:21:02 +0000

    [install robocop]

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -2,16 +2,16 @@ class Admin::RoomsController < ApplicationController
 
   def index
     @chats = Chat.all
-    #@chat = Chat.find(params[:id])
+    # @chat = Chat.find(params[:id])
   end
 
   def show
-      @user = User.find(params[:id])
-      @room = Room.find_by(params[:id])
-    if UserRoom.where(:room_id => @room.id).present? #roomがnilではなかったらなおかつuser_room処理を実行
-      @chat =Chat.new
-      @chats = @room.chats
-  
+    @user = User.find(params[:id])
+    @room = Room.find_by(params[:id])
+    # roomがnilではなかったらなおかつuser_room処理を実行
+    if UserRoom.where(:room_id => @room.id).present?
+     @chat = Chat.new
+     @chats = @room.chats
     else
       redirect_back(fallback_location: root_path)
     end
